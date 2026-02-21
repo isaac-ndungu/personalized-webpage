68829c052d943c002842684f0e0f6c7d420715cc
isaac-ndungu <isaacndungu478@gmail.com>
2026-02-22 00:46:45 +0300

    add javascript to handle storing of user information to localStorage

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+// DOM Elements
+
+let userForm = document.querySelector('.userForm');
+let nameInput = document.querySelector('.nameInput');
+let ageInput = document.querySelector('.ageInput');
+let button = document.querySelector('.button');
+
+// Store input to localStorage
+
+let users =  JSON.parse(localStorage.getItem("users")) || [];
+let addUser = function(name, age) {
+    users.push({name, age})
+    
+    localStorage.setItem('users', JSON.stringify(users));
+
+    return {name, age}
+}
+
+userForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  addUser(nameInput.value, ageInput.value);
+});
+
+
 
