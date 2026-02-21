6f1e20e4f07ebc0f705110ca912bb3d2494e57b4
isaac-ndungu <isaacndungu478@gmail.com>
2026-02-22 01:02:57 +0300

    add conditional logic to verify user age

--- script.js
+++ script.js
@@ -19,7 +19,17 @@ let addUser = function(name, age) {
 userForm.addEventListener('submit', function(e) {
   e.preventDefault();
   addUser(nameInput.value, ageInput.value);
+  ageVerification(ageInput.value);
 });
 
 
+// determine whether the user is old enough for certain content
 
+function ageVerification(age) {
+  let newAge = parseInt(age);
+  if (newAge < 18) {
+    alert('You are too young for adult content');
+  } else {
+    alert('You can access adult content');
+  }
+}
